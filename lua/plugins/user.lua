505876d4c5772fd494f629d868994ea0efae6c8a
yi-huang-1 <yi_huang@student.uml.edu>
2025-07-02 20:34:11 -0400

    feat: add rust-analyzer config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -85,4 +85,21 @@ return {
       )
     end,
   },
+  {
+    "AstroNvim/astrolsp",
+    opts = {
+      config = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+                extraArgs = { "--profile", "rust-analyzer" },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
